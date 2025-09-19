8639fc9420825b35f02f6f523b67719c80087afc
Sandipan Ray <sray387.official@gmail.com>
2025-09-19 16:45:44 +0530

    Add files via upload

---
+++ rect_perimeter.c
@@ -0,0 +1,26 @@
+//Program to print a perimeter of a rectangle using asterisks
+//Program Date: 17-09-2025
+//Name: Sandipan Ray
+//Regn. No.: 2025CA085
+
+#include <stdio.h>
+int main () {
+    for (int i=1; i<=20; i++) {
+        printf("* ");
+    }
+    for (int j=1; j<=14; j++) {
+        printf("\n");
+        printf("\r");
+        printf("*");
+        for (int k=1; k<=37; k++) {
+            printf(" ");
+        }
+        printf("*");
+    }
+    printf("\r");
+    for (int m=1; m<=20; m++) {
+        printf("* ");
+    }
+    printf("\n");
+}
+    
